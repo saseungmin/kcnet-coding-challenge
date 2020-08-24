c408a1b9b33f8c6fb29ba2617e1c70c473a2b737
saseungmin <dbd02169@naver.com>
2020-08-25 00:26:40 +0900

    update Rank css refactoring

--- coding-challenge-frontend/src/components/introduce/RankForm.js
+++ coding-challenge-frontend/src/components/introduce/RankForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Responsive from '../common/Responsive';
-import palette from 'src/lib/styles/palette';
+import RankItem from './RankItem';
 
 const RankFormBlock = styled(Responsive)`
   margin-top: 2rem;
@@ -20,109 +20,42 @@ const RankTitleBlock = styled.div`
 `;
 
 const RankTableBlock = styled.div`
-    margin: 0 0 5rem 0;
-    width: 100%;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.2);
-    display: table;
-    border-collapse: collapse;
-    border-radius: 0.5em;
-    overflow: hidden;
+  margin: 0 0 5rem 0;
+  width: 100%;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
+  display: table;
+  border-collapse: collapse;
+  border-radius: 0.5em;
+  overflow: hidden;
 `;
 
-const RankTableHeaderBlock = styled.div`
-  display: table-row;
-  background: #f6f6f6;
-  &:nth-of-type(odd){
-    background: #e9e9e9;
-  }
-  &.header{
-    font-weight: 900;
-    color: #ffffff;
-    background: ${palette.Teal[5]};
-  }
-`;
-
-const RankRowBlock = styled.div`
-    padding: 6px 12px;
-    display: table-cell;
-    &.text-center{
-    text-align: center;
-  }
-`;
+const fakeData = [
+    {
+        score: 500,
+        lang: 'JavaScript',
+        userid: "seung02169",
+        username: '사승민',
+    },
+    {
+        score: 300,
+        lang: 'JavaScript',
+        userid: "seung02169",
+        username: '사하랑',
+    },
+    {
+        score: 400,
+        lang: 'JavaScript',
+        userid: "seung02169",
+        username: '홍길동',
+    }
+]
 
 const RankForm = () => {
   return (
     <RankFormBlock>
       <RankTitleBlock>랭킹</RankTitleBlock>
       <RankTableBlock>
-          <RankTableHeaderBlock className='header'>
-            <RankRowBlock className='text-center'>
-                순위
-            </RankRowBlock>
-            <RankRowBlock>
-                이름
-            </RankRowBlock>
-            <RankRowBlock>
-                LEVEL
-            </RankRowBlock>
-            <RankRowBlock>
-                언어
-            </RankRowBlock>
-            <RankRowBlock>
-                점수
-            </RankRowBlock>
-          </RankTableHeaderBlock>
-          <RankTableHeaderBlock>
-            <RankRowBlock className='text-center'>
-                1
-            </RankRowBlock>
-            <RankRowBlock>
-                사승민
-            </RankRowBlock>
-            <RankRowBlock>
-                2
-            </RankRowBlock>
-            <RankRowBlock>
-                JavaScript
-            </RankRowBlock>
-            <RankRowBlock>
-                500
-            </RankRowBlock>
-          </RankTableHeaderBlock>
-          <RankTableHeaderBlock>
-            <RankRowBlock className='text-center'>
-                2
-            </RankRowBlock>
-            <RankRowBlock>
-                홍길동
-            </RankRowBlock>
-            <RankRowBlock>
-                1
-            </RankRowBlock>
-            <RankRowBlock>
-                JavaScript
-            </RankRowBlock>
-            <RankRowBlock>
-                400
-            </RankRowBlock>
-          </RankTableHeaderBlock>
-          <RankTableHeaderBlock>
-            <RankRowBlock className='text-center'>
-                3
-            </RankRowBlock>
-            <RankRowBlock>
-                마바아
-            </RankRowBlock>
-            <RankRowBlock>
-                1
-            </RankRowBlock>
-            <RankRowBlock>
-                JavaScript
-            </RankRowBlock>
-            <RankRowBlock>
-                300
-            </RankRowBlock>
-          </RankTableHeaderBlock>
+        <RankItem ranklist={fakeData} />
       </RankTableBlock>
     </RankFormBlock>
   );
--- coding-challenge-frontend/src/components/introduce/RankItem.js
+++ coding-challenge-frontend/src/components/introduce/RankItem.js
@@ -1,35 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
+import palette from 'src/lib/styles/palette';
 
 const RankItemBlock = styled.div`
-    padding: 6px 12px;
-    display: table-cell;
-    &.text-center{
+  padding: 6px 12px;
+  display: table-cell;
+  &.text-center {
     text-align: center;
-    }
+  }
 `;
 
 const RankTableItemBoxBlock = styled.div`
   display: table-row;
   background: #f6f6f6;
-  &:nth-of-type(odd){
+  &:nth-of-type(odd) {
     background: #e9e9e9;
-  }
-  &.header{
-    font-weight: 900;
-    color: #ffffff;
-    background: ${palette.Teal[5]};
   }
 `;
 
-const RankItem= () => {
-    return (
-        <RankTableItemBoxBlock>
-            <RankItemBlock>
-                
-            </RankItemBlock>
+const RankTableTitleBlock = styled.div`
+  display: table-row;
+  font-weight: 900;
+  color: #ffffff;
+  background: ${palette.Teal[5]};
+`;
+
+const RankItem = ({ ranklist }) => {
+  return (
+    <>
+      <RankTableTitleBlock>
+        <RankItemBlock className="text-center">순위</RankItemBlock>
+        <RankItemBlock>이름</RankItemBlock>
+        <RankItemBlock>LEVEL</RankItemBlock>
+        <RankItemBlock>언어</RankItemBlock>
+        <RankItemBlock>점수</RankItemBlock>
+      </RankTableTitleBlock>
+      {ranklist.map((val, index) => 
+        <RankTableItemBoxBlock key={index}>
+          <RankItemBlock className="text-center">{index+1}</RankItemBlock>
+          <RankItemBlock>{`${val.userid}(${val.username})`}</RankItemBlock>
+          <RankItemBlock>2</RankItemBlock>
+          <RankItemBlock>{val.lang}</RankItemBlock>
+          <RankItemBlock>{val.score}</RankItemBlock>
         </RankTableItemBoxBlock>
-    );
+      )}
+    </>
+  );
 };
 
 export default RankItem;
